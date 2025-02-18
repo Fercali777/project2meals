8e54b1b3cd5bd1e1e005041db7ce018ff8d4025d
Fercali777 <fernandocalixto89@gmail.com>
2025-02-18 11:15:37 +0100

    testing not charge design folder

--- js/main.js
+++ js/main.js
@@ -5,15 +5,30 @@ for (let i = 0; i < meals.length; i++) {
   const td1 = document.createElement("td")
   const td2 = document.createElement("td")
   const td3 = document.createElement("td")
+  const td4 = document.createElement("td")
+  const td5 = document.createElement("td")
+  const td6 = document.createElement("td")
+  const td7 = document.createElement("td")
 
 
   td1.innerHTML = meals[i].idMeal
   td2.innerHTML = meals[i].strMeal
   td3.innerHTML = meals[i].strCategory
+  td4.innerHTML = meals[i].strArea
+  td5.innerHTML = meals[i].strTags
+  td6.innerHTML = meals[i].strIngredient1
+  td7.innerHTML = meals[i].strInstructions
+
+
+ 
 
   tr.appendChild(td1)
   tr.appendChild(td2)
   tr.appendChild(td3)
+  tr.appendChild(td4)
+  tr.appendChild(td5)
+  tr.appendChild(td6)
+  tr.appendChild(td7)
 
   tbody.appendChild(tr)
 }
