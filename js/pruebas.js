f30197ce636557fd44eec1c96e9c197782b81287
Fercali777 <fernandocalixto89@gmail.com>
2025-02-21 11:52:10 +0100

    tabla con titulo

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,3 +1,5 @@
+
+
 // table from spike
 // const tbody = document.getElementById("tbody")
 // for (let i = 0; i < meals.length; i++) {
@@ -82,45 +84,48 @@ for (let i = 0 ; i < meals.length ; i++){
 
  
 
-
-
-
-
-
-    console.log ("im looping");
+    
     const tituloRecipe = document.createElement("h1");
     const containerRecipe = document.createElement("div");
     const containerCol = document.createElement("div");
+    const imgRecipe = document.createElement("img");
     const containerCol2 = document.createElement("div");
     const textType = document.createElement("h4");
     const textTypeCont = document.createElement("h5");
     const textIngredients = document.createElement("h4");
     const containerIngredients = document.createElement("div");
-    const imgRecipe = document.createElement("img");
+    const titlePreparation = document.createElement("h4");
+    const textPreparation = document.createElement("p");
+    
 
     tituloRecipe.textContent = meals[i].strMeal;
     containerRecipe.id = "recipe-container";
     containerRecipe.className = "row";
     containerCol.className = "col-3";
+    imgRecipe.src = meals[i].strMealThumb;
     containerCol2.className = "col recipe-text";
     containerCol2.id = "recipe-text";
     textType.textContent = "Type";
     textTypeCont.textContent = meals[i].strCategory + ", " + meals[i].strArea ;
     textIngredients.textContent = "Ingredients";
     containerIngredients.id = "containerIngredients";
+    titlePreparation.textContent = "Preparation";
+    textPreparation.textContent = meals[i].strInstructions; 
     // containerRecipe.textContent = "Here the text";
-    imgRecipe.src = meals[i].strMealThumb;
+    
     
     document.getElementById("contenedorRecipe").appendChild(tituloRecipe);
     document.getElementById("contenedorRecipe").appendChild(containerRecipe);
     document.getElementById("recipe-container").appendChild(containerCol);
-    document.getElementById("recipe-container").appendChild(containerCol2);
     document.getElementById("recipe-container").appendChild(imgRecipe);
+    document.getElementById("recipe-container").appendChild(containerCol2);
     document.getElementById("recipe-text").appendChild(textType);
     document.getElementById("recipe-text").appendChild(textTypeCont);
     document.getElementById("recipe-text").appendChild(textIngredients);
     document.getElementById("recipe-text").appendChild(containerIngredients);
-
+    document.getElementById("recipe-text").appendChild(titlePreparation);
+    document.getElementById("recipe-text").appendChild(textPreparation);
+   
     containerRecipe.appendChild(containerCol);
     containerRecipe.appendChild(containerCol2);
     containerCol.appendChild(imgRecipe);
@@ -134,16 +139,45 @@ for (let i = 0 ; i < meals.length ; i++){
 
 // Ingredients table
 const meal = meals[0]; 
-const ingredients = [];
+const ingredients = [0];
 const measures = [];
 
-for (let i = 1; i <= 20; i++) {
+
+
+for (let i = 1 ; i <= ingredients.length ; i++) {
   const ingredient = meal[`strIngredient${i}`]; 
   const measure = meal[`strMeasure${i}`]; 
+
+
+
   if (ingredient && ingredient !== "") { 
     ingredients.push(ingredient);
     measures.push(measure)
-  }
+  }  
+
+  
+
+
+
+console.log ("Ingredients.length", ingredients.length);  
+
+
+if (i == 1){
+const triT =  document.createElement("tr")
+const thi1 = document.createElement("th")
+const thi2 = document.createElement("th")
+
+
+  thi1.textContent = "Ingredient";
+  thi2.textContent = "Measure";
+  triT.appendChild(thi1)
+  triT.appendChild(thi2)
+  
+  containerIngredients.appendChild(triT)
+}
+
+
+
 
 const tri = document.createElement("tr")
 const tdi1 = document.createElement("td")
@@ -151,8 +185,10 @@ const tdi2 = document.createElement("td")
 
 tdi1.innerHTML = ingredient
 tdi2.innerHTML = measure
+
 tri.appendChild(tdi1)
 tri.appendChild(tdi2)
+
 containerIngredients.appendChild(tri)
 }
 // console.log(ingredients);
